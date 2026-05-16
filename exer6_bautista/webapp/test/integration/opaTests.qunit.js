5cd131c0bc0891134cb2846119e866bfb60caccf
Renz Bautista <renz.l.bautista@accenture.com>
2026-05-16 11:01:30 +0000

    initial commit

---
+++ exer6_bautista/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/training/exer6bautista/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ exer6_bautista/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/training/exer6bautista/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
