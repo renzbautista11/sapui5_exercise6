8e5a594d3c6813c2e53e7c33976c11aeca008d32
Renz Bautista <renz.l.bautista@accenture.com>
2026-05-17 15:32:25 +0000

    Additon of Exercise 6

--- exer6_bautista/webapp/controller/MainView.controller.js
+++ exer6_bautista/webapp/controller/MainView.controller.js
@@ -17,7 +17,27 @@ function (Controller, MessageToast) {
 
         onAddItem: function ()
             {
-                this.fnDisplayMsg("Add button pressed");
+                // Comment this code for now
+                // var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+                // var sMsg = oTextBundle.getText("addButtonMsg");
+                // this.fnDisplayMsg(sMsg);
+
+                // Instantiate the fragment
+
+                // create dialog lazily
+                if (!this.oDialog) {
+                    // By using loadFragment, we are adding the fragment as a dependent to the View
+                    // By doing so, we can use the functions inside the view's controller
+                    this.oDialog = this.loadFragment({
+                        name: "com.training.exer6_bautista.fragment.ProductDialog"
+                    });
+                //this.fnDisplayMsg("Add button pressed1");
+                } 
+                
+                this.oDialog.then(function(oDialog) {
+                    oDialog.open();
+                });
+                //this.fnDisplayMsg("Add button pressed");
             },
 
             fnDisplayMsg: function (sMsg){
@@ -70,16 +90,31 @@ function (Controller, MessageToast) {
 
         onPressCheckout: function ()
             {
-                var oInputFNameValue = this.getView().byId("idInptFName").getValue();
-                var oInputLNameValue = this.getView().byId("idInptLName").getValue();
-                
-                // Check if first name is blank
-                if (oInputFNameValue === ""|| oInputLNameValue === "") {
-                    sap.m.MessageToast.show("Required Field is blank"); 
+                var oInputFName = this.getView().byId("idInptFName");
+                var oInputLName = this.getView().byId("idInptLName");
+                var oInputFNameValue = oInputFName.getValue();
+                var oInputLNameValue = oInputLName.getValue();
+                var oRouter = this.getOwnerComponent().getRouter();
+
+                // Check if first name and last name is blank
+                if (oInputFNameValue === "" || oInputLNameValue === ""){
+                   
+// set value state to Error
+                    oInputFName.setValueState("Error");
+                    oInputLName.setValueState("Error");
+                } else {
+                    oInputFName.setValueState("None");
+                    oInputLName.setValueState("None");
+                    
+                    //Navigate to review page passing first
+                    oRouter.navTo("RouteReviewPage", { firstName: oInputFNameValue  });
+
                 }
+
             },
-
-
+        onCloseDialog: function (){
+                    this.getView().byId("idProductDialog").close();
+                },
 
     });
 });
